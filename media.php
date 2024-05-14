82a6d50255031a85aca8de29845351eb9eaa24e4
robertcosm <robertrobertcosm@gmail.com>
2024-05-14 18:19:21 -0300

    continuando o treinamento de PHP

--- media.php
+++ media.php
@@ -8,19 +8,18 @@
     <?php
 
     echo "<h1> Cálculo das médias dos alunos do 3A do Sesi moreno <h1>";
+    $nome = $_GET['nomeAluno'];
+    $nota1= $_GET['n1'];
+    $nota2= $_GET['n2'];
+    $nota3= $_GET['n3'];
+    echo "<h1>Média do aluno $nome</h1>";
 
-    function calcularMedia($notas) {
-        $soma = array_sum($notas);
-        $quantidade = count($notas);
-        $media = $soma / $quantidade;
-        return $media;
-    }
+    $media = ($nota1 + $nota2 + $nota3) / 3;
+    $mediaArredondada = round($media);
 
-    $notas = array( 8, 7, 10);
-    $media = calcularMedia($notas);
-    echo "A média do aluno é: $media";
+    echo "A média do aluno é: $mediaArredondada";
 
-    if($media>= 8) {
+    if($mediaArredondada>= 8) {
         echo "<p style='color : green'> O aluno foi aprovado";
     }else{
         echo "<p style='color : red'> O aluno foi reprovado";
